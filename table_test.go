f110f1a060b4679325837be51d87a0bca014d04f
Paul Stuart <pauleyphonic@gmail.com>
2015-01-21 17:36:55 -0800

    add test for html links in tables

---
+++ table_test.go
@@ -0,0 +1,19 @@
+package dbutil
+
+import (
+	"testing"
+)
+
+func TestHTML(t *testing.T) {
+	table, err := test_db.Table("select * from foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	table.SetLinks(0, "/x/%s/%s", 0, 0, 1)
+	for row := range table.HTML() {
+		t.Log("ROW")
+		for col := range row.Columns() {
+			t.Log("COL", col)
+		}
+	}
+}
