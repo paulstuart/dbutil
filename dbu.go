89a4621e0a4a40d8858ffa78940e829b3d7c8994
Paul Stuart <pauleyphonic@gmail.com>
2017-08-02 19:53:12 -0700

    group like functions in like files

--- dbu.go
+++ dbu.go
@@ -257,6 +257,53 @@ func keyIsSet(obj interface{}) bool {
 	return false
 }
 
+// generate list of sql fields for members.
+// if skip_key is true, do not include the key field in the list
+func dbFields(obj interface{}, skip_key bool) (table, key, fields string) {
+	t := reflect.TypeOf(obj)
+	list := make([]string, 0, t.NumField())
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if is_table := f.Tag.Get("table"); len(is_table) > 0 {
+			table = is_table
+		}
+		k := f.Tag.Get("sql")
+		if f.Tag.Get("key") == "true" {
+			key = k
+			if skip_key {
+				continue
+			}
+		}
+		if len(k) > 0 {
+			list = append(list, k)
+		}
+	}
+	fields = strings.Join(list, ",")
+	return
+}
+
+// marshal the object fields into an array
+func objFields(obj interface{}, skip_key bool) (interface{}, []interface{}) {
+	val := reflect.ValueOf(obj)
+	t := reflect.TypeOf(obj)
+	a := make([]interface{}, 0, t.NumField())
+	var key interface{}
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if len(f.Tag.Get("sql")) == 0 {
+			continue
+		}
+		if f.Tag.Get("key") == "true" {
+			key = val.Field(i).Interface()
+			if skip_key {
+				continue
+			}
+		}
+		a = append(a, val.Field(i).Interface())
+	}
+	return key, a
+}
+
 func (db DBU) ObjectInsert(obj interface{}) (int64, error) {
 	skip := !keyIsSet(obj) // if we have a key, we should probably use it
 	_, a := objFields(obj, skip)
@@ -310,6 +357,32 @@ func (db DBU) ObjectUpdate(obj interface{}) error {
 
 	_, err := db.Update(query, args...)
 	return err
+}
+
+func deleteInfo(obj interface{}) (table, key string, id interface{}) {
+	val := reflect.ValueOf(obj)
+	t := reflect.TypeOf(obj)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if is_table := f.Tag.Get("table"); len(is_table) > 0 {
+			table = is_table
+		}
+		if len(f.Tag.Get("sql")) == 0 {
+			continue
+		}
+		if f.Tag.Get("update") == "false" {
+			continue
+		}
+		k := f.Tag.Get("sql")
+		v := val.Field(i).Interface()
+		is_key := f.Tag.Get("key")
+		if is_key == "true" {
+			key = k
+			id = v
+			break
+		}
+	}
+	return
 }
 
 func (db DBU) ObjectDelete(obj interface{}) error {
@@ -403,6 +476,20 @@ func (db DBU) GetIDs(query string, args ...interface{}) ([]int64, error) {
 	}
 	rows.Close()
 	return ids, err
+}
+
+// sPtrss makes slice of pointers to struct members for sql scanner
+// expects struct value as input
+func sPtrs(obj interface{}) []interface{} {
+	base := reflect.Indirect(reflect.ValueOf(obj))
+	t := reflect.TypeOf(base.Interface())
+	data := make([]interface{}, 0, base.NumField())
+	for i := 0; i < base.NumField(); i++ {
+		if tag := t.Field(i).Tag.Get("sql"); len(tag) > 0 {
+			data = append(data, base.Field(i).Addr().Interface())
+		}
+	}
+	return data
 }
 
 func (db DBU) ObjectLoad(obj interface{}, extra string, args ...interface{}) (err error) {
@@ -673,4 +760,50 @@ func (db DBU) Version() (int64, error) {
 
 func (d DBU) Exec(query string, args ...interface{}) (affected, last int64, err error) {
 	return Exec(d.DB, query, args...)
+}
+
+func setParams(params string) string {
+	list := strings.Split(params, ",")
+	for i, p := range list {
+		list[i] = fmt.Sprintf("%s=?", p)
+	}
+	return strings.Join(list, ",")
+}
+
+func createQuery(obj interface{}, skip_key bool) string {
+	var table string
+	t := reflect.TypeOf(obj)
+	list := make([]string, 0, t.NumField())
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if len(f.Tag.Get("sql")) == 0 {
+			continue
+		}
+		name := f.Tag.Get("table")
+		if len(name) > 0 {
+			table = name
+		}
+		if skip_key {
+			key := f.Tag.Get("key")
+			if key == "true" {
+				continue
+			}
+		}
+		list = append(list, f.Tag.Get("sql"))
+	}
+	if len(table) == 0 {
+		return ("error: no table name specified for object:" + t.Name())
+	}
+	return "select " + strings.Join(list, ",") + " from " + table
+}
+
+func keyIndex(obj interface{}) int {
+	t := reflect.TypeOf(obj)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if len(f.Tag.Get("key")) > 0 {
+			return i
+		}
+	}
+	return 0 // TODO: error handling!
 }
